e0527ffcce0fab2f4b424618fd66579a7cdf4feb
Chris Choi <mh2choi@uwaterloo.ca>
2016-07-20 23:32:56 -0400

    assigned pins for sensors and actuators

---
+++ Broomba.ino
@@ -0,0 +1,30 @@
+//Broomba Code
+//Created July 20, 2016
+//Terrible Hacks
+// Kenneth Wong, Chris Choi, Nicholas Manna
+
+//digital pins
+#define servoPin   3
+#define echo1      4
+#define trigger1   5
+#define motor1Pin1 6
+#define echo2      7
+#define trigger2   8
+#define motor1Pin2 9
+#define motor2Pin1 10
+#define motor2Pin2 11
+
+//analog pins
+#define lightSensor1 14
+#define lightSensor2 15
+
+//global variables
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  
+
+}
