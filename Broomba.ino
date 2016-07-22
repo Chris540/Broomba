cd05dce6d14cbdf16e8cd94734f5e828dde656b2
Chris Choi <mh2choi@uwaterloo.ca>
2016-07-22 00:15:51 -0400

    added NewPing library, basic movements

--- Broomba.ino
+++ Broomba.ino
@@ -1,3 +1,5 @@
+#include <NewPing.h>
+
 //Broomba Code
 //Created July 20, 2016
 //Terrible Hacks
@@ -19,7 +21,15 @@
 #define lightSensor2 15
 
 //global variables
+//for ultrasonic sensors
 unsigned int usTimeout = 5000;
+byte maxCMDist = 40;
+long duration1 = 0;
+long duration2 = 0;
+byte distInCM1 = 0;
+byte distInCM2 = 0;
+NewPing sonar1(trigger1, echo1, maxCMDist);
+NewPing sonar2(trigger2, echo2, maxCMDist);
 
 void setup() {
   Serial.begin(9600);
@@ -38,6 +48,62 @@ void setup() {
 }
 
 void loop() {
-  
-
+  delay(50);
+  Serial.print("Ultrasonic Sensor 1:");
+  Serial.println(ultrasonic_test1());
+  Serial.print("Ultrasonic Sensor 2:");
+  Serial.println(ultrasonic_test2());
+  rotateCW(500);
+  allStop();
+  delay(2000);
 }
+
+void moveFoward (){
+    analogWrite(motor1Pin1, 180);
+    analogWrite(motor1Pin2, 0);
+    analogWrite(motor2Pin1, 0);
+    analogWrite(motor2Pin2, 180); 
+}
+
+void moveBackward (){
+    analogWrite(motor1Pin1, 0);
+    analogWrite(motor1Pin2, 180);
+    analogWrite(motor2Pin1, 180);
+    analogWrite(motor2Pin2, 0);
+}
+
+void rotateCW (int degree){
+    analogWrite(motor1Pin1, 0);
+    analogWrite(motor1Pin2, 180);
+    analogWrite(motor2Pin1, 0);
+    analogWrite(motor2Pin2, 180);
+    delay(degree);
+}
+
+void rotateCCW (int degree){
+    analogWrite(motor1Pin1, 180);
+    analogWrite(motor1Pin2, 0);
+    analogWrite(motor2Pin1, 180);
+    analogWrite(motor2Pin2, 0);
+    delay(degree);
+}
+
+void allStop(){
+    analogWrite(motor1Pin1, 0);
+    analogWrite(motor1Pin2, 0);
+    analogWrite(motor2Pin1, 0);
+    analogWrite(motor2Pin2, 0);
+}
+
+void finisher () {
+  
+}
+
+byte ultrasonic_test1 () {
+  return (sonar1.ping_cm());
+}
+
+byte ultrasonic_test2 () {
+  return (sonar2.ping_cm());
+}
+
